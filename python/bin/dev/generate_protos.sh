7a933ac9f4064683c3e2cfbcbc3bd4dc50aa3b48
Adrian Miguel <adriantmiguel@gmail.com>
2023-02-01 09:27:29 -0800

    adding generated proto

---
+++ python/bin/dev/generate_protos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ "${0}" != *"bin/dev/generate_protos.sh" ]] || [[ "$(basename $(pwd))" != "python" ]]; then
+  echo "script must be run from the subrepo root: worker-sdk/python"
+  exit 1
+fi
+
+protoc --proto_path=../protobuf --python_out=src/gen ../protobuf/*
