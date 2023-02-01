e6f4059182a41a6c6d45306dd05a40d7f67e953a
Adrian Miguel <adriantmiguel@gmail.com>
2023-02-01 15:13:13 -0800

    works

--- python/src/worker_sdk/messaging/message_serialization.py
+++ python/src/worker_sdk/messaging/message_serialization.py
@@ -3,15 +3,12 @@ from src.gen.test_pb2 import TestMessage
 
 def serialize_thing() -> None:
     tm = TestMessage()
+    # print(tm.HasField("page_number"))
+    tm.SerializeToString()
     tm.page_number = 3
-    print(tm.page_number)
-    print(dir(tm))
-    print(tm.SerializeToString())
-    print(type(tm.SerializeToString()))
-    tm2 = TestMessage()
-    tm2.ParseFromString(tm.SerializeToString())
-
-    print(tm2.page_number)
+    tm.query = "abc"
+    print(tm.ByteSize())
+    tm.SerializeToString()
 
 
 serialize_thing()
