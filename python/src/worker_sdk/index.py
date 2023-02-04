63f38b46623588ec94351b670ff7db7d01529c7b
Adrian Miguel <adriantmiguel@gmail.com>
2023-02-03 16:27:24 -0800

    using single underscore for internal variables

--- python/src/worker_sdk/index.py
+++ python/src/worker_sdk/index.py
@@ -1,73 +1,8 @@
-from typing import Callable, Optional
+from typing import Callable, Dict
 
 
-class PreemoWorker:
-    self.__registered_functions_by_name = {"name": lambda x: x * 2}
-
-    def register(
-        function: Optional[Callable] = None,
-        *,
-        name: Optional[str] = None,
-    ) -> Callable:
-        # TODO(adrian@preemo.io, 02/01/2023): handle namespace as well
-        if function is None:
-            if name is None:
-                raise ValueError("name must be specified")
-
-            function_name = name
-        else:
-            if name is not None:
-                # TODO(adrian@preemo.io, 02/01/2023): clean error messaging
-                raise ValueError("did not expect name")
-
-            function_name = function.__name__
-
-        # TODO(adrian@preemo.io, 02/01/2023): do the registering here
-        print(f"registering func_name: {function_name}")
-
-        # TODO(adrian@preemo.io, 02/02/2023): handle how an execute command is called
-        # The idea here being that we do something like `python user_file.py function_name params_or_path_to_params params2 params3`
-        # Then right here we can check sys.argv to see if this function name matches the one passed in.
-        # If so, execute immediately
-
-        # def decorator_register(func: Callable) -> Callable:
-        #     @functools.wraps(func)
-        #     # TODO(adrian@preemo.io, 02/01/2023): can i clean up these types?
-        #     def wrapper_decorator(*args: Any, **kwargs: Any) -> Any:
-        #         # Do something before
-        #         value = func(*args, **kwargs)
-        #         # Do something after
-        #         return value
-
-        #     return wrapper_decorator
-
-        if function is None:
-            return lambda x: x
-        else:
-            return function
-
-
-register = preemo.register
-
-
-@register
-def say_whee() -> None:
-    print("Whee!")
-
-
-@preemo.register(name="blah")
-def say_whee2() -> None:
-    print("Whee!")
-
-
-say_whee()
-say_whee2()
-
-# preemo.init()
-
-
-def z(*, blah1: int, blah2: int) -> None:
-    pass
-
-
-z(blah1=1, blah2=2)
+class PreemoWorkerClient:
+    def __init__(self) -> None:
+        self._registered_functions_by_namespace_and_name: Dict[
+            str, Dict[str, Callable]
+        ] = {}
--- python/src/worker_sdk/messaging/client.py
+++ python/src/worker_sdk/messaging/client.py
@@ -6,11 +6,11 @@ from src.worker_sdk.environment.manager import get_required_env
 class MessagingClient:
     def __init__(self) -> None:
         context = zmq.Context()
-        self.__socket = context.socket(zmq.REQ)
-        self.__socket.connect(get_required_env("PREEMO_WORKER_SERVER_URL"))
+        self._socket = context.socket(zmq.REQ)
+        self._socket.connect(get_required_env("PREEMO_WORKER_SERVER_URL"))
 
     def send_message(self, message: str) -> str:
-        self.__socket.send_string(message)
-        reply = self.__socket.recv()
+        self._socket.send_string(message)
+        reply = self._socket.recv()
 
         return reply.decode("utf-8")
