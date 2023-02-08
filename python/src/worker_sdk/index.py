e898427e2c201c9785b4c5b8ed17a36b33aff066
Adrian Miguel <adriantmiguel@gmail.com>
2023-02-08 14:23:04 -0800

    moving function registry

--- python/src/worker_sdk/index.py
+++ python/src/worker_sdk/index.py
@@ -1,10 +1,11 @@
-from typing import Callable, Dict, Optional
+from typing import Callable, Optional
 
 from src.gen.worker_request_pb2 import (
     RegisteredFunction,
     RegisterFunctionRequest,
     WorkerRequest,
 )
+from src.worker_sdk.function_registry import FunctionRegistry
 from src.worker_sdk.messaging.client import IMessagingClient
 
 
@@ -20,50 +21,6 @@ def _construct_register_function_worker_request(
     return worker_request
 
 
-class FunctionRegistry:
-    def __init__(self) -> None:
-        self._global_functions_by_name: Dict[str, Callable] = {}
-        self._functions_by_namespace_and_name: Dict[str, Dict[str, Callable]] = {}
-
-    def register_function(
-        self, function: Callable, *, name: str, namespace: Optional[str]
-    ) -> None:
-        if namespace is None:
-            if name in self._global_functions_by_name:
-                raise Exception(
-                    f"must not register multiple functions with the same name: {name}"
-                )
-            self._global_functions_by_name[name] = function
-
-            return
-
-        if namespace not in self._functions_by_namespace_and_name:
-            self._functions_by_namespace_and_name[namespace] = {}
-        functions_by_name = self._functions_by_namespace_and_name[namespace]
-
-        if name in functions_by_name:
-            raise Exception(
-                f"must not register multiple functions with the same namespace: {namespace} and name: {name}"
-            )
-        functions_by_name[name] = function
-
-    def get_function(self, *, name: str, namespace: Optional[str]) -> Callable:
-        if namespace is None:
-            function = self._global_functions_by_name.get(name)
-            if function is None:
-                raise Exception(f"cannot find registered function with name: {name}")
-
-            return function
-
-        function = self._functions_by_namespace_and_name.get(namespace, {}).get(name)
-        if function is None:
-            raise Exception(
-                f"cannot find registered function with namespace: {namespace} and name: {name}"
-            )
-
-        return function
-
-
 class PreemoWorkerClient:
     def __init__(self, *, messaging_client: IMessagingClient) -> None:
         self._client = messaging_client
@@ -77,7 +34,6 @@ class PreemoWorkerClient:
         namespace: Optional[str] = None,
     ) -> Callable:
         def decorator(function: Callable) -> Callable:
-            # TODO(adrian@preemo.io, 02/15/2023): consider trimming and validating string length for name and namespace
             if name is None:
                 function_name = function.__name__
             else:
@@ -93,8 +49,6 @@ class PreemoWorkerClient:
                 )
             )
 
-            # TODO(adrian@preemo.io, 02/15/2023): consider not returning the function
-            # that way users cannot call registered functions when registering
             return function
 
         if outer_function is None:
