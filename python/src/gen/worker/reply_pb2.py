20ab468bd2d088b644aaf043dca59450e9238b1a
Adrian Miguel <adriantmiguel@gmail.com>
2023-02-09 16:56:15 -0800

    moving shared protos

--- python/src/gen/worker/reply_pb2.py
+++ python/src/gen/worker/reply_pb2.py
@@ -11,18 +11,18 @@ from google.protobuf import symbol_database as _symbol_database
 _sym_db = _symbol_database.Default()
 
 
-from shared import status_pb2 as shared_dot_status__pb2
+import shared_pb2 as shared__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x12worker/reply.proto\x1a\x13shared/status.proto\"b\n\x15RegisterFunctionReply\x12\x1c\n\x06status\x18\x01 \x01(\x0e\x32\x07.StatusH\x00\x88\x01\x01\x12\x14\n\x07message\x18\x02 \x01(\tH\x01\x88\x01\x01\x42\t\n\x07_statusB\n\n\x08_message\"K\n\x0bWorkerReply\x12\x33\n\x11register_function\x18\x01 \x01(\x0b\x32\x16.RegisterFunctionReplyH\x00\x42\x07\n\x05replyb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x12worker/reply.proto\x1a\x0cshared.proto\"b\n\x15RegisterFunctionReply\x12\x1c\n\x06status\x18\x01 \x01(\x0e\x32\x07.StatusH\x00\x88\x01\x01\x12\x14\n\x07message\x18\x02 \x01(\tH\x01\x88\x01\x01\x42\t\n\x07_statusB\n\n\x08_message\"K\n\x0bWorkerReply\x12\x33\n\x11register_function\x18\x01 \x01(\x0b\x32\x16.RegisterFunctionReplyH\x00\x42\x07\n\x05replyb\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'worker.reply_pb2', globals())
 if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
-  _REGISTERFUNCTIONREPLY._serialized_start=43
-  _REGISTERFUNCTIONREPLY._serialized_end=141
-  _WORKERREPLY._serialized_start=143
-  _WORKERREPLY._serialized_end=218
+  _REGISTERFUNCTIONREPLY._serialized_start=36
+  _REGISTERFUNCTIONREPLY._serialized_end=134
+  _WORKERREPLY._serialized_start=136
+  _WORKERREPLY._serialized_end=211
 # @@protoc_insertion_point(module_scope)
--- python/src/gen/worker/reply_pb2.pyi
+++ python/src/gen/worker/reply_pb2.pyi
@@ -1,4 +1,4 @@
-from shared import status_pb2 as _status_pb2
+import shared_pb2 as _shared_pb2
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
 from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
@@ -10,8 +10,8 @@ class RegisterFunctionReply(_message.Message):
     MESSAGE_FIELD_NUMBER: _ClassVar[int]
     STATUS_FIELD_NUMBER: _ClassVar[int]
     message: str
-    status: _status_pb2.Status
-    def __init__(self, status: _Optional[_Union[_status_pb2.Status, str]] = ..., message: _Optional[str] = ...) -> None: ...
+    status: _shared_pb2.Status
+    def __init__(self, status: _Optional[_Union[_shared_pb2.Status, str]] = ..., message: _Optional[str] = ...) -> None: ...
 
 class WorkerReply(_message.Message):
     __slots__ = ["register_function"]
