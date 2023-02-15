b2b6c4d0c83ce3c21b4132791936aa0ccdf7cac7
Adrian Miguel <adriantmiguel@gmail.com>
2023-02-15 11:26:04 -0800

    fixing scripts

--- python/bin/dev/format.sh
+++ python/bin/dev/format.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -x
 
-# TODO(adrian@preemo.io, 02/13/2023): test if these commands work
-isort preemo src tests
-autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place --ignore-init-module-imports preemo src tests
-black preemo src tests
+isort preemo tests
+autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place --ignore-init-module-imports preemo tests
+black preemo tests
--- python/bin/dev/lint.sh
+++ python/bin/dev/lint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -x
 
-# TODO(adrian@preemo.io, 02/13/2023): check if these work as intended
-mypy preemo src tests
-black --check preemo src tests
-isort --check-only preemo src tests
-flake8 preemo src tests
+mypy preemo tests
+black --check preemo tests
+isort --check-only preemo tests
+flake8 preemo tests
