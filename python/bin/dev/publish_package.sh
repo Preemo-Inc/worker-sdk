b22e48bd37326705ce017e073e35b670bf386b87
Adrian Miguel <adriantmiguel@gmail.com>
2023-02-15 17:36:59 -0800

    using pypirc

--- python/bin/dev/publish_package.sh
+++ python/bin/dev/publish_package.sh
@@ -6,23 +6,13 @@ if [[ "${0}" != *"bin/dev/publish_package.sh" ]] || [[ "$(basename $(pwd))" != "
   exit 1
 fi
 
-# TODO(adrian@preemo.io, 02/15/2023): consider adding link to pypi env file
-if [ -f pypi.env ]; then
-  source pypi.env
-fi
-
-if [ -z "${PYPI_USERNAME}" ] || [ -z "${PYPI_PASSWORD}" ]; then
-  echo "missing at least one required env variable: PYPI_USERNAME, PYPI_PASSWORD"
-  exit 1
-fi
-
 pytest tests
 rm -rf dist
 python3 -m build
 
+# must have valid ~/.pypirc file to upload
 # TODO(adrian@preemo.io, 02/15/2023): remove the repository line
 python3 -m twine upload \
-  --username "${PYPI_USERNAME}" \
-  --password "${PYPI_PASSWORD}" \
+  --non-interactive \
   --repository testpypi \
   dist/*
