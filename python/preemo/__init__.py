18fc643e883bb21fc3d09df709a5b5f0a67c80bd
Adrian Miguel <adriantmiguel@gmail.com>
2023-02-15 16:01:35 -0800

    saving

--- python/preemo/__init__.py
+++ python/preemo/__init__.py
@@ -1,3 +1,4 @@
 from importlib import metadata as _metadata
 
-__version__ = _metadata.version("preemo_worker_sdk")
+# TODO(adrian@preemo.io, 02/15/2023): revert this back
+__version__ = _metadata.version("preemo_worker_sdk3")
