28b4a6e16f109d1bb61053bdb5e8d959d7805bbe
Adrian Miguel <adriantmiguel@gmail.com>
2023-03-16 15:47:39 -0700

    cleaning worker client

--- python/preemo/worker/_worker_client.py
+++ python/preemo/worker/_worker_client.py
@@ -7,11 +7,7 @@ from preemo.gen.endpoints.execute_function_pb2 import ExecuteFunctionRequest
 from preemo.gen.endpoints.register_function_pb2 import RegisterFunctionRequest
 from preemo.gen.models.registered_function_pb2 import RegisteredFunction
 from preemo.gen.models.value_pb2 import Value
-from preemo.worker._artifact_manager import (
-    ArtifactId,
-    ArtifactManager,
-    IArtifactManager,
-)
+from preemo.worker._artifact_manager import ArtifactId, IArtifactManager
 from preemo.worker._function_registry import FunctionRegistry
 from preemo.worker._messaging_client import IMessagingClient
 
@@ -26,7 +22,7 @@ class Function:
         namespace: Optional[str],
     ) -> None:
         self._artifact_manager = artifact_manager
-        self._client = messaging_client
+        self._messaging_client = messaging_client
         self.name = name
         self.namespace = namespace
 
@@ -34,7 +30,7 @@ class Function:
 
     def _ensure_function_is_registered(self) -> None:
         # check_function raises an error if the function is not found
-        self._client.check_function(
+        self._messaging_client.check_function(
             CheckFunctionRequest(
                 function_to_check=RegisteredFunction(
                     name=self.name, namespace=self.namespace
@@ -49,7 +45,7 @@ class Function:
             artifact_id = self._artifact_manager.create_artifact(params)
             function_parameter = Value(artifact_id=artifact_id.value)
 
-        response = self._client.execute_function(
+        response = self._messaging_client.execute_function(
             ExecuteFunctionRequest(
                 function_to_execute=RegisteredFunction(
                     name=self.name, namespace=self.namespace
@@ -74,49 +70,18 @@ class Function:
 
 
 class WorkerClient:
-    def __init__(self, *, messaging_client: IMessagingClient) -> None:
-        self._client = messaging_client
-        # TODO(adrian@preemo.io, 03/15/2023): figure out if artifact manager should be passed in?
-        # needs to depend on the same messaging client though
-        self._artifact_manager = ArtifactManager(messaging_client=messaging_client)
+    def __init__(
+        self, *, artifact_manager: IArtifactManager, messaging_client: IMessagingClient
+    ) -> None:
+        self._artifact_manager = artifact_manager
+        self._messaging_client = messaging_client
+
         self._function_registry = FunctionRegistry()
-
-    def register(
-        self,
-        outer_function: Optional[Callable] = None,
-        *,
-        name: Optional[str] = None,
-        namespace: Optional[str] = None,
-    ) -> Callable:
-        def decorator(function: Callable) -> Callable:
-            if name is None:
-                function_name = function.__name__
-            else:
-                function_name = name
-
-            self._function_registry.register_function(
-                function, name=function_name, namespace=namespace
-            )
-
-            self._client.register_function(
-                RegisterFunctionRequest(
-                    function_to_register=RegisteredFunction(
-                        name=function_name, namespace=namespace
-                    )
-                )
-            )
-
-            return function
-
-        if outer_function is None:
-            return decorator
-
-        return decorator(outer_function)
 
     def get_function(self, name: str, *, namespace: Optional[str] = None) -> Function:
         return Function(
             artifact_manager=self._artifact_manager,
-            messaging_client=self._client,
+            messaging_client=self._messaging_client,
             name=name,
             namespace=namespace,
         )
@@ -128,7 +93,7 @@ class WorkerClient:
         params: Optional[list[str]] = None,
         count: Optional[int] = None,
     ) -> list[Optional[str]]:
-        # TODO(adrian@preemo.io, 03/08/2023): should take an optional config argument including batching?
+        # TODO(adrian@preemo.io, 03/20/2023): should take an optional config argument includes stuff like max batch size
 
         if params is None:
             if count is None:
@@ -153,7 +118,7 @@ class WorkerClient:
                 for i, artifact_id in enumerate(artifact_ids)
             }
 
-        response = self._client.execute_function(
+        response = self._messaging_client.execute_function(
             ExecuteFunctionRequest(
                 function_to_execute=RegisteredFunction(
                     name=function.name, namespace=function.namespace
@@ -162,9 +127,8 @@ class WorkerClient:
             )
         )
 
+        # TODO(adrian@preemo.io, 03/20/2023): should download results in parallel or return a handle that allows the user to download result
         results: list[Optional[str]] = []
-        # TODO(adrian@preemo.io, 03/15/2023): should download results in parallel
-        # or just return a handle that allows the user to download
         for _, function_result in sorted(
             response.results_by_index.items(), key=lambda x: x[0]
         ):
@@ -183,3 +147,35 @@ class WorkerClient:
                     )
 
         return results
+
+    def register(
+        self,
+        outer_function: Optional[Callable] = None,
+        *,
+        name: Optional[str] = None,
+        namespace: Optional[str] = None,
+    ) -> Callable:
+        def decorator(function: Callable) -> Callable:
+            if name is None:
+                function_name = function.__name__
+            else:
+                function_name = name
+
+            self._function_registry.register_function(
+                function, name=function_name, namespace=namespace
+            )
+
+            self._messaging_client.register_function(
+                RegisterFunctionRequest(
+                    function_to_register=RegisteredFunction(
+                        name=function_name, namespace=namespace
+                    )
+                )
+            )
+
+            return function
+
+        if outer_function is None:
+            return decorator
+
+        return decorator(outer_function)
